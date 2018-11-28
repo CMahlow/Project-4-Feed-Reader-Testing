f15883d7351a58ab3a12215888a7e08fcc4213d7
Coding Mommy <christymahlow@gmail.com>
2018-11-28 14:56:00 -0800

    Removed 'the' next to menu

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -56,7 +56,7 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
-    describe('The menu',function(){
+    describe('menu',function(){
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
